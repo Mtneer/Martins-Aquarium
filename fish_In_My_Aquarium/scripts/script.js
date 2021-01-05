c0823bba3ce5e14a4944e753b8925c42d94ead97
David Billups <davidtbillups@gmail.com>
2021-01-05 14:16:25 -0500

    Worked through the CSS layout for fish in my aquarium page

---
+++ fish_In_My_Aquarium/scripts/script.js
@@ -0,0 +1,44 @@
+let fishCards = [
+    {
+        imgFile = "https://static.wikia.nocookie.net/pixar/images/1/1f/Dory-white.jpg",
+        name = "Dory",
+        size = 3, // in inches
+        locHarvest = "P Sherman 42 Wallaby Way Sydney",
+        food = "Pretzels"
+    },
+    {
+        imgFile = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixar.fandom.com%2Fwiki%2FHank&psig=AOvVaw2H3hYJ0MGNjaxQUHatT6GY&ust=1609960158882000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjEs42_he4CFQAAAAAdAAAAABAD",
+        name = "Hank",
+        size = 1, // in inches
+        locHarvest = "Monterey Bay Acquarium",
+        food = "Twinkies"
+    },
+    {
+        imgFile = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffindingdory.fandom.com%2Fwiki%2FDestiny&psig=AOvVaw2mR-vRs5LIjzXMQ9JINZLl&ust=1609960378194000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDF1fW_he4CFQAAAAAdAAAAABAD",
+        name = "Destiny",
+        size = 1, // in inches
+        locHarvest = "Monterey Bay Acquarium",
+        food = "Krill"
+    },
+    {
+        imgFile = "",
+        name = "Bruce",
+        size = 1, // in inches
+        locHarvest = "The open ocean",
+        food = "Fish"
+    },
+    {
+        imgFile = "",
+        name = "Nigel",
+        size = 1, // in inches
+        locHarvest = "Sydney, Australia",
+        food = "Crabs"
+    },
+    {
+        imgFile = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixar.fandom.com%2Fwiki%2FBubbles&psig=AOvVaw1Yum6IhLENQl2s5OiZZkZA&ust=1609960193941000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCID9uKC_he4CFQAAAAAdAAAAABAD",
+        name = "Bubbles",
+        size = 1, // in inches
+        locHarvest = "42 Wallaby Way Sydney",
+        food = "Bubbles"
+    }
+]
