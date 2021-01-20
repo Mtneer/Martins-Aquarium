f91714ed653f45817bb7843eff36393606393526
David Billups <davidtbillups@gmail.com>
2021-01-20 10:54:37 -0500

    refactored data provider to include filtering based on Martin's desires

--- scripts/fish/fishData.js
+++ scripts/fish/fishData.js
@@ -2,42 +2,42 @@ let fishCards = [
     {
         imgFile: "https://static.wikia.nocookie.net/pixar/images/1/1f/Dory-white.jpg",
         name: "Dory",
-        size: "3 inches",
+        size: 3,
         locHarvest: "P Sherman 42 Wallaby Way Sydney",
         food: "Pretzels"
     },
     {
         imgFile: "https://static.wikia.nocookie.net/pixar/images/d/de/Hank_FD.jpg",
         name: "Hank",
-        size: "3 feet",
+        size: 36,
         locHarvest: "Monterey Bay Acquarium",
         food: "Twinkies"
     },
     {
         imgFile: "https://vignette.wikia.nocookie.net/findingdory/images/6/6c/Destiny_render.png/revision/latest?cb=20160520041520",
         name: "Destiny",
-        size: "20 feet",
+        size: 240,
         locHarvest: "Monterey Bay Acquarium",
         food: "Krill"
     },
     {
         imgFile: "https://static.wikia.nocookie.net/pixar/images/8/83/71423763-2085-4F98-8461-FC6E2C48828E.png",
         name: "Bruce",
-        size: "15 feet",
+        size: 180,
         locHarvest: "The open ocean",
         food: "Fish"
     },
     {
         imgFile: "https://static.wikia.nocookie.net/disneyssebastiandory/images/7/7f/Nigel.png",
         name: "Nigel",
-        size: "3 feet",
+        size: 36,
         locHarvest: "Sydney, Australia",
         food: "Crabs"
     },
     {
         imgFile: "https://static.wikia.nocookie.net/pixar/images/0/0c/Bubbles.png",
         name: "Bubbles",
-        size: "3 inches",
+        size: 3,
         locHarvest: "42 Wallaby Way Sydney",
         food: "Bubbles"
     }
@@ -46,3 +46,60 @@ let fishCards = [
 export const fishList = () => {
     return fishCards.slice()
 }
+
+export const mostHolySoldierFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holySoldierFish = [];
+
+    for (let i=0; i<fishCards.length; i++) {
+        if (fishCards[i].size % 3 === 0 && fishCards[i].size % 5 === 0) {
+            holySoldierFish.push(fishCards[i]);
+        }
+    }
+
+    return holySoldierFish
+}
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = [];
+
+    for (let i=0; i<fishCards.length; i++) {
+        if (fishCards[i].size % 3 === 0 && fishCards[i].size % 5 === 0) {
+
+        } else if (fishCards[i].size % 3 === 0) {
+            holyFish.push(fishCards[i]);
+        }
+    }
+
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiers = [];
+
+    for (let i=0; i<fishCards.length; i++) {
+        if (fishCards[i].size % 3 === 0 && fishCards[i].size % 5 === 0) {
+            
+        } else if (fishCards[i].size % 5 === 0) {
+            holyFish.push(fishCards[i]);
+        }
+    }
+
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const regularFish = [];
+
+    for (let i=0; i<fishCards.length; i++) {
+        if (fishCards[i].size % 3 !== 0 && fishCards[i].size % 5 !== 0) {
+            regularFish.push(fishCards[i]);
+        }
+    }
+
+    return regularFish
+}
+
--- scripts/fish/fishHtmlGenerator.js
+++ scripts/fish/fishHtmlGenerator.js
@@ -1,11 +1,17 @@
-import {fishList} from './fishData.js'
+import {fishList, mostHolySoldierFish, mostHolyFish, soldierFish, nonHolyFish} from './fishData.js'
+
 import {printFishDetail} from './fishHtmlCard.js'
 
 // Select the div where we want to print details about the fish
 let fishDetails = document.querySelector(".fishCards")
 
 // loop through all the party guests
-const fishObject = fishList();
+let fishObject = mostHolySoldierFish();
+fishObject = fishObject.concat(mostHolyFish());
+fishObject = fishObject.concat(soldierFish());
+fishObject = fishObject.concat(nonHolyFish());
+
+console.log(fishObject)
 
 export function fishHtmlString () {
     for (const fish of fishObject) {
